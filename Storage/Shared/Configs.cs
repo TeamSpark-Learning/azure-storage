099df8d01fe7ee1766d1ec40a86d3b4ae2124851
Anton Boyko <me@boykoant.pro>
2018-12-14 23:50:10 +0200

    cleanup

--- Storage/Shared/Configs.cs
+++ Storage/Shared/Configs.cs
@@ -4,7 +4,7 @@ namespace Shared
 {
     public static class Configs
     {
-        public static string ConnectionString => "DefaultEndpointsProtocol=https;AccountName=valtechdemo02;AccountKey=jRAUmBCc1VdCWzdRbBK7YPpfyOT6B1SxWC/NbdmSu5V43oVYKjRZlKf5s8BoPDmOqzNgVewkW3fgvgADbxUV2w==;EndpointSuffix=core.windows.net";
+        public static string ConnectionString => "";
 
         public static int PID => Process.GetCurrentProcess().Id;
     }
